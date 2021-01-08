6c7e258c1691f320f81cca23d668aa864d07d7d9
Cubiix7 <pierre.coupechoux@laposte.net>
2021-01-08 16:05:56 +0100

    08/01 soir

--- books/urls.py
+++ books/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='books_index'),
+	path('list/', views.list_view, name='books_list'),
+	path('detail/<int:book_id>', views.detail_view, name='books_detail'),
+	path('lent/', views.lent_view),
 ]
--- books/views.py
+++ books/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book
 
 # Create your views here.
 def index(request):
-	return HttpResponse('Hello world!')
+	return render(request,'books/index.html')
+	
+def list_view(request):
+	b = Book.objects.get(title='Harry Potter 1')
+	
+	context_dict = {'book':b}
+	return render(request, 'books/list.html', context_dict)
+	
+def detail_view(request, book_id):
+	return HttpResponse(f"Détail du livre {book_id}.")
+	
+def lent_view(request):
+	return HttpResponse('Ceci est la liste des livres indisponibles.')
